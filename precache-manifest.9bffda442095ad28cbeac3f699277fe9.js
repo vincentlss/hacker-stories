9428dd4e3aedceec9cdd3cee5e1ef9639da056e4
vincentlss <shyanshinvincent@gmail.com>
2020-06-10 15:08:13 +0800

    Updates

---
+++ precache-manifest.9bffda442095ad28cbeac3f699277fe9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5b726c66966a479b78e421f87d0c060",
+    "url": "/hacker-stories/index.html"
+  },
+  {
+    "revision": "25f1d3e8da855b74c6ac",
+    "url": "/hacker-stories/static/css/main.b98a0041.chunk.css"
+  },
+  {
+    "revision": "b93dcbd6ff60a79affbe",
+    "url": "/hacker-stories/static/js/2.3740dc52.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/hacker-stories/static/js/2.3740dc52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25f1d3e8da855b74c6ac",
+    "url": "/hacker-stories/static/js/main.53850ef9.chunk.js"
+  },
+  {
+    "revision": "b89b617c5ec0473315fb",
+    "url": "/hacker-stories/static/js/runtime-main.df8346ba.js"
+  },
+  {
+    "revision": "28e6eff3123e64ade2cb1088da721fdb",
+    "url": "/hacker-stories/static/media/check.28e6eff3.svg"
+  }
+]);
